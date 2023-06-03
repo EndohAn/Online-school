5ffcdbeaf5cd9d661b075aca8ecdd4588e75c01b
unknown <flowerrosa76@gmail.com>
2023-06-03 13:13:25 +0900

    update.v

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 AOS.init();
 
 
-// $(function(){
-//     $(window).scroll(function(){
-//         if($(this).scrollTop()> 1600){
-//             $('.pageup').fadeIn();
-//         }else{
-//             $('.pageup').fadeOut();
-//         }    
-//     });
 
-//     $("a[href^=#up]").click(function(){
-//         const _href = $(this).attr("href");
-//         $("html,body").animate({scrollTop:$(_href).offset().top+"px"});
-//         return false;
-//     });
-//  });
 // стрелка вверх
 
  window.addEventListener('scroll', function () {
@@ -42,12 +28,3 @@ window.addEventListener('scroll',() => {
 })
 
 
-
-// // gsap.from('.navbar-brand',{duration:3,opacity:0})
-// // gsap.from('.title',{y:100,duration:2,delay:0.6,opacity:0})
-// gsap.from('.nav-item ',{duration:3,opacity:0,stagger:1}) 
-// // gsap.from('.button',{scale:1.7,duration:3,repeat:-1})
-// gsap.from('.img-about',{x:500,duration:2,delay:0.8,opacity:0})
-// gsap.from('.img-wrapper-info',{x:500,duration:2,delay:0.8,opacity:0})
-// gsap.from('.img-wrapper-info-two',{x:-500,duration:2,delay:0.8,opacity:0})
-// gsap.from('.img-wrapper-info-three',{x:500,duration:2,delay:0.8,opacity:0})
